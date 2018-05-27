8d924ab4b50fc4c794aecbe37b39a37a48260f32
letangers <2910787416@qq.com>
2018-05-28 02:30:21 +0800

    增加了对SIGINT信号的处理 发现了一个bug 当未输入任何字符就按下回车之后，以后的输入就不起作用了。 目前我猜测可能是cin.getline（）函数的问题

--- client/client.cpp
+++ client/client.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-#include <string.h>
+#include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+
+#include <csignal>
 using namespace std;
 
+int sock;
+
+void handle(int sig){
+	close(sock);
+	cout<<"exit by ctrl c"<<endl;
+	exit(EXIT_SUCCESS);
+}
 
 int main(void){
-	int sock;
+	if(signal(SIGINT,handle)<0)
+		cerr<<"something wrong when installing signal"<<endl;
+
 	if((sock=socket(PF_INET,SOCK_STREAM,IPPROTO_TCP))<0)
 		cout<<"something wrong when creating socket"<<endl;
 	struct sockaddr_in cliaddr;
@@ -21,11 +32,16 @@ int main(void){
 	char sendbuf[1024]={0};
 	char recvbuf[1024]={0};
 	while(cin.getline(sendbuf,1024)){
+		if(strcmp(sendbuf,"exit")==0){
+			cout<<"exit"<<endl;
+			break;
+		}
 		write(sock,sendbuf,strlen(sendbuf));
 		read(sock,recvbuf,sizeof(recvbuf));
-		cout<<"[receive from server]"<<recvbuf;
+		cout<<"[receive from server]"<<recvbuf<<endl;
 		memset(sendbuf,0,sizeof(sendbuf));
 		memset(recvbuf,0,sizeof(recvbuf));
+		cin.clear();
 	}
 	close(sock);
 	return 0;
