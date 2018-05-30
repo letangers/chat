33bff2f87cdef0fc17243812695773a10f258aa3
hilbrtanger <2910787416@qq.com>
2018-05-30 19:40:09 +0800

    将多进程改为多线程 每到来一个连接，开一个线程进行发送和接收数据 退出时关闭套接字

--- server/server.cpp
+++ server/server.cpp
@@ -5,7 +5,33 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <cstdlib>
+
+#include <pthread.h>
 using namespace std;
+
+
+void * send_and_recv(void * arg){
+	char recvbuf[1024];
+	while (true){
+		memset(recvbuf,0,sizeof(recvbuf));
+		int ret=read(*((int *)arg),recvbuf,sizeof(recvbuf));
+		if(ret==0){
+			cout<<"connection break  "<<*((int *)arg)<<endl;
+			break;
+		}
+		else if (ret==-1){
+			cerr<<"recv buf failed  "<<*((int *)arg)<<endl;
+			break;
+		}
+		     else 
+			cout<<"everything is ok  "<<*((int *)arg)<<endl;
+		cout<<recvbuf<<endl;
+		write(*((int *)arg),recvbuf,ret);
+	}
+	close(*((int *)arg));
+	return (void*)0;
+}
+
 
 int main(void){
 	int listenfd;
@@ -29,41 +55,16 @@ int main(void){
 	
 	struct sockaddr_in peeraddr;
 	socklen_t peerlen=sizeof(peeraddr);
-	int conn;
 
-	pid_t pid;
 	while(true){
- 	if((conn=accept(listenfd,(struct sockaddr*)&peeraddr,&peerlen))<0)
+	int *conn=new int;
+ 	if((*conn=accept(listenfd,(struct sockaddr*)&peeraddr,&peerlen))<0)
 		cerr<<"something wrong when accept"<<endl;
 	cout<<"ip:"<<inet_ntoa(peeraddr.sin_addr)<<"  port:"<<ntohs(peeraddr.sin_port)<<endl;
         
-	pid=fork();
-	if(pid==-1)
-		cerr<<"fork is wrong"<<endl;
-	if(pid==0)
-	{
-		close(listenfd);	
-
-		char recvbuf[1024];
-		while (true){
-			memset(recvbuf,0,sizeof(recvbuf));
-			int ret=read(conn,recvbuf,sizeof(recvbuf));
-			if(ret==0){
-				cout<<"connection break"<<endl;
-				close(conn);
-				exit(EXIT_SUCCESS);
-			}
-			else if (ret==-1)
-				cerr<<"recv buf failed"<<endl;
-			     else 
-				cout<<"everything is ok"<<endl;
-			cout<<recvbuf<<endl;
-			write(conn,recvbuf,ret);
-		}
-		exit(EXIT_SUCCESS);
-	}
-	else
-		close(conn);
+	pthread_t tid;
+	if(pthread_create(&tid,NULL,send_and_recv,conn)<0)
+		cerr<<"create thread failed"<<endl;
 	}
 	close(listenfd);
 	return 0;
