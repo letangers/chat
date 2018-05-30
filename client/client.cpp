d04a9d03acce1cd0314434aa17895f0ef3ff8383
letangers <2910787416@qq.com>
2018-05-30 18:02:56 +0800

    更改了一下代码结构

--- client/client.cpp
+++ client/client.cpp
@@ -1,3 +1,5 @@
+#include "init.cpp"
+
 #include <iostream>
 #include <cstring>
 #include <sys/socket.h>
@@ -6,23 +8,48 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <csignal>
+
 using namespace std;
 
-int sock;
+pid_t pid;
 
-void handle(int sig){
+void send_to_service(){
+	if(signal(SIGINT,handle)<0)
+		cerr<<"something wrong when installing sub signal"<<endl;
+	if(signal(SIGUSR1,endprocess)<0)
+		cerr<<"something wrong when installing sub SIGUSR1"<<endl;;
+	char sendbuf[1024]={0};
+	while(cin.getline(sendbuf,1024)){
+		if(strcmp(sendbuf,"exit")==0){
+			cout<<"exit"<<endl;
+			kill(getppid(),SIGUSR1);
+			break;
+		}
+		send(sock,sendbuf,strlen(sendbuf),0);
+		memset(sendbuf,0,sizeof(sendbuf));
+	}
+	kill(getppid(),SIGUSR1);
 	close(sock);
-	cout<<"recv signal SIGINT"<<endl;
-	exit(EXIT_SUCCESS);
 }
-
-void endprocess(int sig){
+void recv_from_service(){
+	if(signal(SIGINT,handle)<0)
+		cerr<<"something wrong when installing signal"<<endl;
+	if(signal(SIGUSR1,endprocess)<0)
+		cerr<<"something wrong when installing SIGUSR1"<<endl;
+	char recvbuf[1024]={0};
+	while(true){
+		int ret=recv(sock,recvbuf,sizeof(recvbuf),0);
+		if(ret==0)
+		{
+			cout<<"服务端断开了连接"<<endl;
+			kill(pid,SIGUSR1);
+			break;
+		}
+		cout<<"[receive from server] "<<recvbuf<<endl;
+		memset(recvbuf,0,sizeof(recvbuf));
+	}
 	close(sock);
-	cout<<"recv signal SIGUSR1"<<endl;
-	exit(EXIT_SUCCESS);
-
 }
-
 
 int main(void){
 
@@ -36,8 +63,6 @@ int main(void){
 	if(connect(sock,(struct sockaddr*)&cliaddr,sizeof(cliaddr))<0)
 		cout<<"something wrong when connect"<<endl;
 	
-	
-	pid_t pid;
 	pid=fork();
 	if (pid==-1){
 		cout<<"create sub process faile"<<endl;
@@ -45,42 +70,10 @@ int main(void){
 	}
 	
 	if(pid==0){
-		if(signal(SIGINT,handle)<0)
-			cerr<<"something wrong when installing sub signal"<<endl;
-		if(signal(SIGUSR1,endprocess)<0)
-			cerr<<"something wrong when installing sub SIGUSR1"<<endl;;
-		char sendbuf[1024]={0};
-		while(cin.getline(sendbuf,1024)){
-			if(strcmp(sendbuf,"exit")==0){
-				cout<<"exit"<<endl;
-				kill(getppid(),SIGUSR1);
-				break;
-			}
-			send(sock,sendbuf,strlen(sendbuf),0);
-			memset(sendbuf,0,sizeof(sendbuf));
-		
-		}
-		kill(getppid(),SIGUSR1);
-		close(sock);
+		send_to_service();
 	}
 	else{
-		if(signal(SIGINT,handle)<0)
-			cerr<<"something wrong when installing signal"<<endl;
-		if(signal(SIGUSR1,endprocess)<0)
-			cerr<<"something wrong when installing SIGUSR1"<<endl;
-		char recvbuf[1024]={0};
-		while(true){
-			int ret=recv(sock,recvbuf,sizeof(recvbuf),0);
-			if(ret==0)
-			{
-				cout<<"服务端断开了连接"<<endl;
-				kill(pid,SIGUSR1);
-				break;
-			}
-			cout<<"[receive from server] "<<recvbuf<<endl;
-			memset(recvbuf,0,sizeof(recvbuf));
-		}
-		close(sock);
+		recv_from_service();
 	}
 	return 0;
 }
---
+++ client/init.cpp
@@ -0,0 +1,20 @@
+#include <csignal>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+using namespace std;
+int sock;
+
+void handle(int sig){
+	close(sock);
+	cout<<"recv signal SIGINT"<<endl;
+	exit(EXIT_SUCCESS);
+}
+
+void endprocess(int sig){
+	close(sock);
+	cout<<"recv signal SIGUSR1"<<endl;
+	exit(EXIT_SUCCESS);
+}
