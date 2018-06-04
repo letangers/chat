83c063e6ac270dad3110d882a2d96b2b9aacde80
letangers <2910787416@qq.com>
2018-06-05 00:01:21 +0800

    增加了对命令的解析

--- client/client.cpp
+++ client/client.cpp
@@ -1,4 +1,5 @@
-#include "init.cpp"
+#include "init.h"
+#include "parse.h"
 #include <iostream>
 #include <cstring>
 #include <sys/socket.h>
@@ -11,20 +12,62 @@
 #include <pthread.h>
 using namespace std;
 
+string sendbuf;
+string cmdline;
 
-void *send_to_service(void *arg){
+int execute_showcommand(){
+	cout<<">>>show online"<<endl<<'\t';
+	cout<<"you can browse user list online"<<endl<<endl;
+	cout<<">>>sendto sb"<<endl<<'\t';
+	cout<<"you can send message to someone online,if the arg is all,send the message to all online"<<endl;
+	
+	return 0;
+}
+
+int execute(){
+	if (cmd=="show"){
+		sendbuf="showonline |";
+		return 0;
+	}
+	if (cmd=="sendto")
+		if(arg=="all"){
+			sendbuf="sendto all|";
+			return 0;
+		}
+		else{
+			cout<<"input the message to "<<arg<<">>>";
+			cmdline="";
+			getline(cin,cmdline);
+			sendbuf="sendto "+arg+"|"+cmdline;
+			return 0;
+		}
+	return -1;
+}
+
+
+void *send_to_service(void *args){
 	if(signal(SIGINT,handle)<0)
 		cerr<<"something wrong when installing sub signal"<<endl;
 	if(signal(SIGUSR1,endprocess)<0)
 		cerr<<"something wrong when installing sub SIGUSR1"<<endl;;
-	char sendbuf[1024]={0};
-	while(cin.getline(sendbuf,1024)){
-		if(strcmp(sendbuf,"exit")==0){
+	while(getline(cin,cmdline)){
+		sendbuf="";
+		cmdline="";
+		parse_command(&cmdline);
+		if (cmd=="exit"){
 			cout<<"exit"<<endl;
 			break;
 		}
-		send(sock,sendbuf,strlen(sendbuf),0);
-		memset(sendbuf,0,sizeof(sendbuf));
+		if (cmd=="showcommand"){
+			execute_showcommand();
+			continue;
+		}
+		if (execute()<0){
+			cout<<"command wrong"<<endl;
+			continue;
+		}
+		
+		send(sock,sendbuf.c_str(),sendbuf.length(),0);
 	}
 	close(sock);
 	exit(EXIT_SUCCESS);
@@ -38,12 +81,17 @@ void recv_from_service(){
 	char recvbuf[1024]={0};
 	while(true){
 		int ret=recv(sock,recvbuf,sizeof(recvbuf),0);
+		if(ret==-1){
+			cout<<"接收失败"<<endl;
+			break;
+		}
 		if(ret==0)
 		{
 			cout<<"服务端断开了连接"<<endl;
 			break;
 		}
-		cout<<"[receive from server] "<<recvbuf<<endl;
+		parse_server(recvbuf);
+		cout<<"[receive from "<<cmd<<"] "<<arg<<endl;
 		memset(recvbuf,0,sizeof(recvbuf));
 	}
 	close(sock);
